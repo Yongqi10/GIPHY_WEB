c2f36d0fb61440081c2b32e9b54fedb992537723
Yongqi10 <q865984106@yahoo.com>
2022-01-11 21:11:08 -0500

    finish Regular and Trending Search

--- src/ListOfGif.js
+++ src/ListOfGif.js
@@ -1,11 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,Fragment } from 'react';
+import "./ListOfGif.css"
+function ListOfGif(typeOfSearch) {
 
-function ListOfGif() {
+  //  const [flag, setFlag] = useState(()=>{return 0})
+    let Search = ""
+    console.log(typeOfSearch.content);
+    switch(typeOfSearch.content){
 
-    const API = "http://api.giphy.com/v1/gifs/search?q=SEARCH+TERM+GOES+HERE&api_key="+ process.env.REACT_APP_API_KEY;
+        case'Regular':
+            Search = "http://api.giphy.com/v1/gifs/search?q=SEARCH+TERM+GOES+HERE&api_key=";
+            console.log("Regular");
+          //  setFlag(0);
+            break;
+        case'Trending':
+            Search = "http://api.giphy.com/v1/gifs/trending?api_key=";
+            console.log("Trending");
+           // setFlag(0);
+            break;
+        case 'Random':
+            Search = "http://api.giphy.com/v1/gifs/random?api_key=";
+            console.log("Random");
+            //setFlag(1);
+            break;
+        default:
+            break;
+    }
+
+    const API = Search + process.env.REACT_APP_API_KEY+ "&limit=15";
+
 
     const [gif,setGif] = useState(()=>{return []})
     const [Loading,setLoading] = useState(() => {return true})
+
+
+
+
     const GiphyAPI = async () => {
         try {
             const response = await fetch(API)
@@ -20,18 +49,46 @@ function ListOfGif() {
         }
 
     }
+
     useEffect (() =>{  
         GiphyAPI()
         
-    },[])
+    },[API])
+
+
+
 
     console.log(gif)
-    return (<li>
-        {
-           Loading === false?  <img src={gif[0].images.downsized_medium.url}></img>: <p>Loading</p>
-        }
+
+    if(typeof(gif) === 'object')
+    {
+        return (
+            <Fragment>
+            {
+                
+               (Loading === false)?
+               gif.map((item,index)=>{
+                return    <li className='m-1 list' key={item+index} > <img className='border border-5 ' src={item.images.downsized_medium.url} alt= ""></img></li>
+               })  
+               : 
+               <p>Loading</p>
+            }
+            </Fragment>
+        );
+    }
+    else {
+        return(
+
+            (Loading === false)?
+             <li className='m-1 list'> <img className='border border-5 ' src={gif.images.downsized_medium.url}></img></li>
+            : 
+            <p>Loading</p>
+            
+        )
         
-    </li>);
+    }
+
+
 }
 
 export default ListOfGif;
