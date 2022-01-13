8b9a4ba25eaa224cd9e87287272b0de189f5ee62
Yongqi10 <q865984106@yahoo.com>
2022-01-12 20:21:18 -0500

    finish search and fix the random button

--- src/ListOfGif.js
+++ src/ListOfGif.js
@@ -1,94 +1,122 @@
-import React, { useState, useEffect,Fragment } from 'react';
-import "./ListOfGif.css"
+import React, { useState, useEffect, useRef, Fragment } from "react";
+import "./ListOfGif.css";
 function ListOfGif(typeOfSearch) {
 
-  //  const [flag, setFlag] = useState(()=>{return 0})
-    let Search = ""
-    console.log(typeOfSearch.content);
-    switch(typeOfSearch.content){
 
-        case'Regular':
-            Search = "http://api.giphy.com/v1/gifs/search?q=SEARCH+TERM+GOES+HERE&api_key=";
-            console.log("Regular");
-          //  setFlag(0);
-            break;
-        case'Trending':
-            Search = "http://api.giphy.com/v1/gifs/trending?api_key=";
-            console.log("Trending");
-           // setFlag(0);
-            break;
-        case 'Random':
-            Search = "http://api.giphy.com/v1/gifs/random?api_key=";
-            console.log("Random");
-            //setFlag(1);
-            break;
-        default:
-            break;
-    }
+  const flag = useRef(0);
+  const current = flag.current;
+  let searchInput = typeOfSearch.searchInput;
+  let Search = "http://api.giphy.com/v1/gifs/search?q=SEARCH+TERM+GOES+HERE&api_key=";
 
-    const API = Search + process.env.REACT_APP_API_KEY+ "&limit=15";
+  switch (typeOfSearch.content) {
+    case "Regular":
+      Search =
+        "http://api.giphy.com/v1/gifs/search?q=SEARCH+TERM+GOES+HERE&api_key=";
+      flag.current = 0;
+      break;
+    case "Trending":
+      Search = "http://api.giphy.com/v1/gifs/trending?api_key=";
+      flag.current = 0;
+      break;
+    case "Random":
+      flag.current = 1;
+      break;
+    default:
+      break;
+  }
 
 
-    const [gif,setGif] = useState(()=>{return []})
-    const [Loading,setLoading] = useState(() => {return true})
-
-
-
-
-    const GiphyAPI = async () => {
-        try {
-            const response = await fetch(API)
-            const data = await response.json()
-            setGif(data.data)
-            setLoading(false)
-
-        }
-        catch(err)
-        {
-            console.error(err.message)
-        }
-
-    }
-
-    useEffect (() =>{  
-        GiphyAPI()
-        
-    },[API])
-
-
-
-
-    console.log(gif)
-
-    if(typeof(gif) === 'object')
+  if(typeOfSearch.content === 'Regular'){
+    if(searchInput !== '')
     {
-        return (
-            <Fragment>
-            {
-                
-               (Loading === false)?
-               gif.map((item,index)=>{
-                return    <li className='m-1 list' key={item+index} > <img className='border border-5 ' src={item.images.downsized_medium.url} alt= ""></img></li>
-               })  
-               : 
-               <p>Loading</p>
-            }
-            </Fragment>
-        );
+      searchInput.replace(' ','+')
+      Search = "http://api.giphy.com/v1/gifs/search?q="+searchInput+"&api_key=";
+      console.log(searchInput)
     }
-    else {
-        return(
+  }
 
-            (Loading === false)?
-             <li className='m-1 list'> <img className='border border-5 ' src={gif.images.downsized_medium.url}></img></li>
-            : 
-            <p>Loading</p>
-            
-        )
-        
+ 
+
+  const API = Search + process.env.REACT_APP_API_KEY + "&limit=15";
+  const APIFromRandom = "http://api.giphy.com/v1/gifs/random?api_key="+process.env.REACT_APP_API_KEY + "&limit=15";
+
+  const [gif, setGif] = useState(() => {
+    return [];
+  });
+  const [Random, setRandom] = useState(() => {
+    return [];
+  });
+  const [Loading, setLoading] = useState(() => {
+    return true;
+  });
+
+
+
+  const GiphyAPI = async () => {
+    try {
+      const response = await fetch(API);
+      const data = await response.json();
+      setGif(data.data);
+      setLoading(false);
+    } catch (err) {
+      console.error(err.message);
     }
+  };
 
 
+  const RandomAIP = async () => {
+    try {
+      const response = await fetch(APIFromRandom);
+      const data = await response.json()
+      setRandom(data.data)
+      setLoading(false)
+    }
+    catch(err){
+      console.error(err)
+    }
+  }
+
+
+  useEffect(() => {
+    RandomAIP();
+    GiphyAPI();
+  }, [API]);
+
+
+  if (current === 0) {
+    return (
+      <Fragment>
+        {Loading === false ? (
+          gif.map((item, index) => {
+            return (
+              <li className="m-1 list" key={item + index}>
+                <img
+                  className="border border-5 "
+                  src={item.images.downsized_medium.url}
+                  alt=""
+                ></img>
+              </li>
+            );
+          })
+        ) : (
+          <p>Loading</p>
+        )}
+      </Fragment>
+    );
+  } else {
+    return current === 1 ? (
+      Loading === false ? (
+        <li className="m-1 list">
+          <img
+            className="border border-5 "
+            src={Random.images.downsized_medium.url}
+          ></img>
+        </li>
+      ) : (
+        <p>Loading</p>
+      )
+    ) : null;
+  }
 }
 
 export default ListOfGif;
